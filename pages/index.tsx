d4369f41562e9d25bddfb09d6f96ee17c8fca304
Berrones Edwin <Berrones.Edwin@hotmail.com>
2023-07-12 16:26:20 -0600

    feat add form to get the username

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,25 +1,48 @@
+import { data } from "@/data/data"
+import { useUsername } from "@/hooks/useUsername"
+import { useRouter } from "next/router"
 import { useState } from "react"
 
 export default function Home() {
   const [user, setUser] = useState("")
+  const [error, setError] = useState("")
 
-  const handleSubmit = (e) =>{
+  const router = useRouter()
+  const {setUsername} = useUsername()
+
+  const handleSubmit = (e) => {
     e.preventDefault()
-    console.log(user)
+    const newUser = data.find(d => d.username === user)
+    setUsername(user)
+    
+    if (!newUser) {
+      // redirect dashboard
+
+      router.push("/dashboard")
+    } else {
+      // show error
+      setError("The username has already chosen, please try another")
+    }
   }
 
   return (
     <main style={{ backgroundColor: "#ccc", minWidth: '100vh' }}>
       <article>
         <form onSubmit={handleSubmit}>
-          <input type="text" 
-                  autoComplete="off" 
-                  required 
-                  placeholder="@my_user" 
-                  value={user} 
-                  onChange={(e) => setUser(e.target.value)} />
+          <input type="text"
+            autoComplete="off"
+            required
+            placeholder="@my_user"
+            value={user}
+            onChange={(e) => setUser(e.target.value)} />
           <button>Register</button>
         </form>
+      </article>
+      {/* Error */}
+      <article>
+        {
+          error.length === 0 ? null : <p>{error}</p>
+        }
       </article>
     </main>
   )
